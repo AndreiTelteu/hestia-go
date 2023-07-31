d545be0a793f9c24e941c47a177af9cc748897e8
Andrei Telteu <andrei@telteu.ro>
2023-07-31 20:51:03 +0300

    Working solidjs/vite in docker with nginx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,7 +9,7 @@ const App: Component = () => {
       <header class={styles.header}>
         <img src={logo} class={styles.logo} alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Hi from docker via proxy
         </p>
         <a
           class={styles.link}
